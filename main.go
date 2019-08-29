c9e465bae57e1c68d869a50177a042d70e45e7aa
konstantin.shevchuk <konstantin.shevchuk@eriks.com>
2019-08-29 20:07:45 +0200

    Implement real call.

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 import "github.com/kostyashevchuk/dockerify/Swagger"
 
 func main() {
-	fmt.Print("Hello")
-	client := Swagger.Client{Server: "http://localhost:8080", Client: http.Client{}}
-	response, err := client.CreateApp(context.Background(), Swagger.CreateAppJSONRequestBody{})
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtyYXNuaWtvdi0tMzU0YWZmMSIsImlhdCI6MTU2NzEwMTc0MSwiZXhwIjoxNTY3MTQ0OTQxLCJhdWQiOiJodHRwczovL2FwaS5kb2NrZXJpemUuaW8iLCJpc3MiOiJEb2NrZXJpemUgTExDIiwic3ViIjoiIn0.1HdVcssKZBwvruIhdE9mEE5jsreKHSIPXBKT080UrAg"
+	client := Swagger.Client{Server: "https://api.dockerize.io", Client: http.Client{},
+		RequestEditor: func(req *http.Request, ctx context.Context) error {
+			req.Header.Add("Authorization", "Bearer "+token)
+			return nil
+		}}
+	response, err := client.GetDockerCredentials(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(response.Body)
+	fmt.Println(response.Status)
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
 }
