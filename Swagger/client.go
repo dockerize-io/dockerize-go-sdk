b2beeff970439ecdab7cd4339ab5e7aa2b88b482
konstantin.shevchuk <konstantin.shevchuk@eriks.com>
2019-08-29 19:38:17 +0200

    tmp fix.

--- Swagger/client.go
+++ Swagger/client.go
@@ -12,7 +12,7 @@ import (
 	"fmt"
 	"github.com/deepmap/oapi-codegen/pkg/runtime"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
 	"io"
 	"io/ioutil"
 	"net/http"
